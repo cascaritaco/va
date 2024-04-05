de524eb702fa7dc5651b4ae58e45383864c41ba4
Jen-464 <jenlopez464@gmail.com>
2024-04-05 08:34:02 -0700

    feat: updated TypeScript syntax

---
+++ src/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+
+  return (
+    <h1 className="text-3xl font-bold underline bg-my_purple">
+    Hello world!
+  </h1>
+  )
+}
+
+export default Home
