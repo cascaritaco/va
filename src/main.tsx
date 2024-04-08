7385e7470793a7348c44c4d21aeee0e3dcd6c082
Jen-464 <jenlopez464@gmail.com>
2024-04-08 15:00:33 -0700

    feat: updated file names

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './Home.tsx'
+import App from './App.tsx'
 import "./main.css"
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>,
 )
