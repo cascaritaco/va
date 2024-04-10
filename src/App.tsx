aa3796d5766d3ea17c3299fa8c1554d8efba6507
Jen-464 <jenlopez464@gmail.com>
2024-04-10 09:08:52 -0700

    feat: updated function name in file

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Nav from "./components/Nav"
 import Banner from "./components/Banner"
+import Table from "./components/Table"
 
-const Home = () => {
+const App = () => {
 
   return (
     <div>
       <Nav/>
       <Banner/>
+      <Table/>
     </div>
   )
 }
 
-export default Home
+export default App
