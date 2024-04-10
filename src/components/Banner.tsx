4d3b9b8adc6766c73b0470df70a3d9ea84055da4
Jen-464 <jenlopez464@gmail.com>
2024-04-10 09:09:08 -0700

    feat: updated side spacing

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,12 +1,11 @@
 import React from "react"
-import BannerImage from "./../assets/background/scattered-forcefields.svg"
 
 const Banner = () => {
     return (
-    <header className="h-44 bg-no-repeat bg-cover bg-center" style={{ backgroundImage: `url(${BannerImage})` }}>
-        <div className="flex flex-col h-full justify-between px-4 xl:px-16">
+    <header className="h-44 bg-url bg-no-repeat bg-cover bg-center bg-[url('src/assets/background/scattered-forcefields.svg')]"> 
+        <div className="flex flex-col h-full justify-between mx-auto max-w-7xl px-4">
             <div></div>
-            <h1 className="text-primaryB font-bold text-h2 xl:text-h1"> Tables </h1>
+            <h1 className="text-primaryB font-bold text-h2 lg:text-h1"> Tables </h1>
             <div className="max-w-fit">
                 <div role="tablist" className="tabs tabs-lifted">
                     <a role="tab" className="tab [--tab-border-color:transparent] tab-active">Tab 1</a>
